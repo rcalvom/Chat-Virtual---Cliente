defeec011f0896ebe7740f3f70425da31e938f69
Ricardo Andrés Calvo <42346347+rcalvom@users.noreply.github.com>
2019-09-14 12:58:25 -0500

    Primer avance GUI

--- Chat Virtual - Cliente/LoginWindow.cs
+++ Chat Virtual - Cliente/LoginWindow.cs
@@ -9,8 +9,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Chat_Virtual___Cliente {
-    public partial class Form1 : Form {
-        public Form1() {
+    public partial class LoginWindow : Form {
+        public LoginWindow() {
             InitializeComponent();
         }
     }
