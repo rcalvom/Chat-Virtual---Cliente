7b654714f32cc5e61142f10be21d751feb6a719b
Samuel Gonzalez <sagonzalezn@unal.edu.co>
2019-09-19 17:00:35 -0500

    Inicio Interfaz Grafica

--- Chat Virtual - Cliente/GraphicInterface.Designer.cs
+++ Chat Virtual - Cliente/GraphicInterface.Designer.cs
@@ -24,12 +24,227 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            BunifuAnimatorNS.Animation animation1 = new BunifuAnimatorNS.Animation();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GraphicInterface));
+            BunifuAnimatorNS.Animation animation3 = new BunifuAnimatorNS.Animation();
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.Restaurar = new System.Windows.Forms.PictureBox();
+            this.Close = new System.Windows.Forms.PictureBox();
+            this.Minimize = new System.Windows.Forms.PictureBox();
+            this.Maximize = new System.Windows.Forms.PictureBox();
+            this.Menu = new System.Windows.Forms.PictureBox();
+            this.MainPanel = new System.Windows.Forms.Panel();
+            this.DragControl = new Bunifu.Framework.UI.BunifuDragControl(this.components);
+            this.Animation = new BunifuAnimatorNS.BunifuTransition(this.components);
+            this.Animation2 = new BunifuAnimatorNS.BunifuTransition(this.components);
+            this.SlidePanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Restaurar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Maximize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Menu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.Indigo;
+            this.TopPanel.Controls.Add(this.Restaurar);
+            this.TopPanel.Controls.Add(this.Close);
+            this.TopPanel.Controls.Add(this.Minimize);
+            this.TopPanel.Controls.Add(this.Maximize);
+            this.TopPanel.Controls.Add(this.Menu);
+            this.Animation2.SetDecoration(this.TopPanel, BunifuAnimatorNS.DecorationType.None);
+            this.Animation.SetDecoration(this.TopPanel, BunifuAnimatorNS.DecorationType.None);
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(1000, 73);
+            this.TopPanel.TabIndex = 0;
+            // 
+            // Restaurar
+            // 
+            this.Restaurar.AccessibleName = "Restore";
+            this.Restaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Animation.SetDecoration(this.Restaurar, BunifuAnimatorNS.DecorationType.None);
+            this.Animation2.SetDecoration(this.Restaurar, BunifuAnimatorNS.DecorationType.None);
+            this.Restaurar.Image = global::Chat_Virtual___Cliente.Properties.Resources.Restore_Window_2_48px;
+            this.Restaurar.Location = new System.Drawing.Point(902, 16);
+            this.Restaurar.Name = "Restaurar";
+            this.Restaurar.Size = new System.Drawing.Size(40, 40);
+            this.Restaurar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Restaurar.TabIndex = 3;
+            this.Restaurar.TabStop = false;
+            this.Restaurar.Visible = false;
+            this.Restaurar.Click += new System.EventHandler(this.PictureBox5_Click);
+            // 
+            // Close
+            // 
+            this.Close.AccessibleName = "Close";
+            this.Close.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Animation.SetDecoration(this.Close, BunifuAnimatorNS.DecorationType.None);
+            this.Animation2.SetDecoration(this.Close, BunifuAnimatorNS.DecorationType.None);
+            this.Close.Image = global::Chat_Virtual___Cliente.Properties.Resources.Close_Window__2_48px;
+            this.Close.Location = new System.Drawing.Point(948, 16);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(40, 40);
+            this.Close.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Close.TabIndex = 1;
+            this.Close.TabStop = false;
+            this.Close.Click += new System.EventHandler(this.PictureBox3_Click);
+            // 
+            // Minimize
+            // 
+            this.Minimize.AccessibleName = "Minimize";
+            this.Minimize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Animation.SetDecoration(this.Minimize, BunifuAnimatorNS.DecorationType.None);
+            this.Animation2.SetDecoration(this.Minimize, BunifuAnimatorNS.DecorationType.None);
+            this.Minimize.Image = global::Chat_Virtual___Cliente.Properties.Resources.Minimize_Window_2_48px;
+            this.Minimize.Location = new System.Drawing.Point(856, 16);
+            this.Minimize.Name = "Minimize";
+            this.Minimize.Size = new System.Drawing.Size(40, 40);
+            this.Minimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Minimize.TabIndex = 2;
+            this.Minimize.TabStop = false;
+            this.Minimize.Click += new System.EventHandler(this.PictureBox4_Click);
+            // 
+            // Maximize
+            // 
+            this.Maximize.AccessibleName = "Maximize";
+            this.Maximize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Animation.SetDecoration(this.Maximize, BunifuAnimatorNS.DecorationType.None);
+            this.Animation2.SetDecoration(this.Maximize, BunifuAnimatorNS.DecorationType.None);
+            this.Maximize.Image = global::Chat_Virtual___Cliente.Properties.Resources.Maximize_Window_2_48px;
+            this.Maximize.Location = new System.Drawing.Point(902, 16);
+            this.Maximize.Name = "Maximize";
+            this.Maximize.Size = new System.Drawing.Size(40, 40);
+            this.Maximize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Maximize.TabIndex = 0;
+            this.Maximize.TabStop = false;
+            this.Maximize.Click += new System.EventHandler(this.PictureBox2_Click);
+            // 
+            // Menu
+            // 
+            this.Animation.SetDecoration(this.Menu, BunifuAnimatorNS.DecorationType.None);
+            this.Animation2.SetDecoration(this.Menu, BunifuAnimatorNS.DecorationType.None);
+            this.Menu.Image = global::Chat_Virtual___Cliente.Properties.Resources.Menu_48px;
+            this.Menu.Location = new System.Drawing.Point(26, 16);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(35, 35);
+            this.Menu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Menu.TabIndex = 0;
+            this.Menu.TabStop = false;
+            this.Menu.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.Animation2.SetDecoration(this.MainPanel, BunifuAnimatorNS.DecorationType.None);
+            this.Animation.SetDecoration(this.MainPanel, BunifuAnimatorNS.DecorationType.None);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(250, 73);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Size = new System.Drawing.Size(750, 527);
+            this.MainPanel.TabIndex = 2;
+            // 
+            // DragControl
+            // 
+            this.DragControl.Fixed = true;
+            this.DragControl.Horizontal = true;
+            this.DragControl.TargetControl = this.TopPanel;
+            this.DragControl.Vertical = true;
+            // 
+            // Animation
+            // 
+            this.Animation.AnimationType = BunifuAnimatorNS.AnimationType.HorizSlide;
+            this.Animation.Cursor = null;
+            animation1.AnimateOnlyDifferences = true;
+            animation1.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.BlindCoeff")));
+            animation1.LeafCoeff = 0F;
+            animation1.MaxTime = 1F;
+            animation1.MinTime = 0F;
+            animation1.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicCoeff")));
+            animation1.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation1.MosaicShift")));
+            animation1.MosaicSize = 0;
+            animation1.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            animation1.RotateCoeff = 0F;
+            animation1.RotateLimit = 0F;
+            animation1.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.ScaleCoeff")));
+            animation1.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation1.SlideCoeff")));
+            animation1.TimeCoeff = 0F;
+            animation1.TransparencyCoeff = 0F;
+            this.Animation.DefaultAnimation = animation1;
+            // 
+            // Animation2
+            // 
+            this.Animation2.AnimationType = BunifuAnimatorNS.AnimationType.HorizSlide;
+            this.Animation2.Cursor = null;
+            animation3.AnimateOnlyDifferences = true;
+            animation3.BlindCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.BlindCoeff")));
+            animation3.LeafCoeff = 0F;
+            animation3.MaxTime = 1F;
+            animation3.MinTime = 0F;
+            animation3.MosaicCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.MosaicCoeff")));
+            animation3.MosaicShift = ((System.Drawing.PointF)(resources.GetObject("animation3.MosaicShift")));
+            animation3.MosaicSize = 0;
+            animation3.Padding = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            animation3.RotateCoeff = 0F;
+            animation3.RotateLimit = 0F;
+            animation3.ScaleCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.ScaleCoeff")));
+            animation3.SlideCoeff = ((System.Drawing.PointF)(resources.GetObject("animation3.SlideCoeff")));
+            animation3.TimeCoeff = 0F;
+            animation3.TransparencyCoeff = 0F;
+            this.Animation2.DefaultAnimation = animation3;
+            // 
+            // SlidePanel
+            // 
+            this.SlidePanel.BackColor = System.Drawing.Color.Black;
+            this.Animation2.SetDecoration(this.SlidePanel, BunifuAnimatorNS.DecorationType.None);
+            this.Animation.SetDecoration(this.SlidePanel, BunifuAnimatorNS.DecorationType.None);
+            this.SlidePanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.SlidePanel.Location = new System.Drawing.Point(0, 73);
+            this.SlidePanel.Name = "SlidePanel";
+            this.SlidePanel.Size = new System.Drawing.Size(250, 527);
+            this.SlidePanel.TabIndex = 1;
+            // 
+            // GraphicInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.MainPanel);
+            this.Controls.Add(this.SlidePanel);
+            this.Controls.Add(this.TopPanel);
+            this.Animation.SetDecoration(this, BunifuAnimatorNS.DecorationType.None);
+            this.Animation2.SetDecoration(this, BunifuAnimatorNS.DecorationType.None);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "GraphicInterface";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.GraphicInterface_Load);
+            this.TopPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Restaurar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Close)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minimize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Maximize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Menu)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.PictureBox Menu;
+        private System.Windows.Forms.Panel MainPanel;
+        private System.Windows.Forms.PictureBox Minimize;
+        private System.Windows.Forms.PictureBox Close;
+        private System.Windows.Forms.PictureBox Maximize;
+        private System.Windows.Forms.PictureBox Restaurar;
+        private Bunifu.Framework.UI.BunifuDragControl DragControl;
+        private BunifuAnimatorNS.BunifuTransition Animation;
+        private BunifuAnimatorNS.BunifuTransition Animation2;
+        private System.Windows.Forms.FlowLayoutPanel SlidePanel;
     }
 }
 
--- Chat Virtual - Cliente/GraphicInterface.cs
+++ Chat Virtual - Cliente/GraphicInterface.cs
@@ -39,5 +39,59 @@ namespace Chat_Virtual___Cliente {
 
         }
 
+        private void GraphicInterface_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+            if ( SlidePanel.Width == 250)
+            {
+                SlidePanel.Visible = false;
+                SlidePanel.Width = 68;
+                Animation.Show(SlidePanel);
+            }
+            else
+            {
+                SlidePanel.Visible = false;
+                Animation2.Show(SlidePanel);
+            }
+        }
+
+        private void PictureBox3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PictureBox4_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void PictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            Maximize.Visible = false;
+            Restaurar.Visible = true;
+        }
+
+        private void PictureBox5_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            Restaurar.Visible = false;
+            Maximize.Visible = true;
+            
+        }
+
+        private void Slider_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
